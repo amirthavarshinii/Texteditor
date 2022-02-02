346943260c5dc61633c4b38fade1d55b1a1383ca
amirthavarshinii <67734492+amirthavarshinii@users.noreply.github.com>
2022-02-01 18:11:09 -0800

    Update Document.java

--- Text_editor/src/document/Document.java
+++ Text_editor/src/document/Document.java
@@ -2,7 +2,6 @@ package document;
 
 /** 
  * A class that represents a text document
- * @author UC San Diego Intermediate Programming MOOC team
  */
 import java.util.ArrayList;
 import java.util.List;
